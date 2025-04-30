6d68acb269cbef5a248d12ec0924d0d081718e49
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-04-30 19:31:33 +0600

    Create Output.py

---
+++ Output.py
@@ -0,0 +1,5 @@
+Tokens: ['Natural', 'Language', 'Processing', 'is', 'fun', 'and', 'exciting.', 'It', 'helps', 'machines', 'understand', 'human', 'language', '.']
+Word Frequency: Counter({'Natural
+￼
+
+Tokens: ['Natural', 'Language', 'Processing', 'is', 'fun', 'and', 'exciting.', 'It', 'helps', 'machines', 'understand', 'human', 'language', '.'] Word Frequency: Counter({'Natural': 1, 'Language': 1, 'Processing': 1, 'is': 1, 'fun': 1, 'and': 1, 'exciting.': 1, 'It': 1, 'helps': 1, 'machines': 1, 'understand': 1, 'human': 1, 'language': 1, '.': 1})
